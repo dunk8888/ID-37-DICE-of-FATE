fee3014575241944052a12fdfb2b88863176e27e
JO3RI <info@JO3RI.be>
2016-04-19 20:27:06 +0200

    flip left right

--- DICE_DEV/menu.h
+++ DICE_DEV/menu.h
@@ -13,20 +13,21 @@ void stateMenuIntro()
 void stateMenuMain()
 {
   arduboy.drawBitmap(0, 0, titleScreen, 128, 64, WHITE);
-  if (buttons.justPressed(LEFT_BUTTON) && (menuSelection < 5)) menuSelection++;
-  if (buttons.justPressed(RIGHT_BUTTON) && (menuSelection > 2)) menuSelection--;
+  if (buttons.justPressed(RIGHT_BUTTON) && (menuSelection < 5)) menuSelection++;
+  if (buttons.justPressed(LEFT_BUTTON) && (menuSelection > 2)) menuSelection--;
   if (buttons.justPressed(B_BUTTON)) gameState = menuSelection;
 }
 
 void stateMenuHelp()
 {
+  arduboy.drawBitmap(32, 0, qrcode, 64, 64, WHITE);
   if (buttons.justPressed(A_BUTTON | B_BUTTON)) gameState = STATE_MENU_MAIN;
 }
 
 
 void stateMenuInfo()
 {
-  arduboy.drawBitmap(32, 0, qrcode, 64, 64, WHITE);
+  
   if (buttons.justPressed(A_BUTTON | B_BUTTON)) gameState = STATE_MENU_MAIN;
 }
 
